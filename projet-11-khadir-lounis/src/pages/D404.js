4514ec3eff807d4bbc545634676c4089784e3014
squal94 <lounis.khadir@wanadoo.fr>
2022-09-01 16:00:34 +0200

    Création de la page 404

--- projet-11-khadir-lounis/src/pages/D404.js
+++ projet-11-khadir-lounis/src/pages/D404.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import { NavLink } from "react-router-dom";
 
 const D404 = () => {
   return (
     <div>
       <Nav />
+      <main className="main__404">
+        <h1 className="main__404--title">404</h1>
+        <p className="main__404--txt">
+          Oups! la page que vous demandez n'existe pas.
+        </p>
+        <NavLink className="main__404--btn" to="/">
+          Retourner sur la page accueil
+        </NavLink>
+      </main>
+      <Footer />
     </div>
   );
 };
